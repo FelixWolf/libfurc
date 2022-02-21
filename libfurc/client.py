e4ed697d8f845f656344f060fea529a61d633271
Kyler Eastridge <felix.wolfz@gmail.com>
2022-02-21 17:15:37 -0500

    more stuff

--- libfurc/client.py
+++ libfurc/client.py
@@ -3,6 +3,7 @@ import asyncio
 
 LIVE_SERVER = ("lightbringer.furcadia.com", 6500)
 
+#Incoming message definitions
 class DefaultPacketHandler:
     async def message_unhandled(self, *args, **kwargs):
         pass
@@ -44,6 +45,7 @@ class PacketHooks(DefaultPacketHandler):
             return
         self.listeners[name].remove(name)
 
+#Outgoing message definitions
 class Commands:
     async def login(self):
         pass
@@ -64,15 +66,37 @@ class Client(PacketHooks, Commands):
         self.writer = None
         self.server = server or LIVE_SERVER
     
+    #Basic networking stuff
     async def connect(self, server = None):
+        if self.connected():
+            await self.disconnect()
         if not server:
             server = self.server
         
         self.reader, self.writer = await asyncio.open_connection(
             server[0], server[1])
+        
         eventLoop = loop.create_task(self.readLoop())
         return eventLoop
     
+    async def disconnect(self):
+        writer.close()
+        await writer.wait_closed()
+    
+    async def send(self, data):
+        if self.connected():
+            await writer.write(data+b"\n")
+            await writer.drain()
+            return True
+        return False
+    
+    @property
+    def connected(self):
+        if self.reader == None or self.writer == None:
+            return False
+        return True
+    
+    #Actual read loop, it is designed to be it's own task
     async def readLoop():
         while True:
             if not self.reader:
