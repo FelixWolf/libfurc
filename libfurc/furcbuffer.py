625b339680a18c2a82b9ca72998d22bdba9d92ea
Kyler Eastridge <felix.wolfz@gmail.com>
2023-01-17 12:50:06 -0500

    Add Write functions to FurcBuffer

--- libfurc/furcbuffer.py
+++ libfurc/furcbuffer.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3 
 from . import base
 
+#TODO: Convert to bytesIO?
 class FurcBuffer:
-    def __init__(self, buffer):
+    def __init__(self, buffer = None):
+        if buffer == None:
+            buffer = b""
         self.buffer = buffer
         self.offset = 0
-
+    
+    def __bytes__(self):
+        return self.buffer
+    
     @property
     def eof(self):
         return self.offset >= len(self.buffer)
@@ -13,6 +19,8 @@ class FurcBuffer:
     @property
     def remaining(self):
         return len(self.buffer) - self.offset
+    
+    
     
     def read(self, l = None):
         if l == None:
@@ -23,8 +31,17 @@ class FurcBuffer:
             self.offset += l
         return v
     
+    def write(self, data):
+        self.buffer = self.buffer[:self.offset] + data + self.buffer[self.offset + len(data):]
+        self.offset += len(data)
+    
+    
     def readBuffer(self, l = 1):
         return FurcBuffer(self.read(l))
+    
+    def writeBuffer(self, v):
+        self.write(v.buffer)
+    
     
     def readUntil(self, seperator = b" "):
         data = b""
@@ -35,17 +52,17 @@ class FurcBuffer:
             data += c
         return data
     
+    
+    
     def read95(self, l = 1):
         try:
             return base.b95decode(self.read(l))
         except ValueError as e:
             raise ValueError("Failed to decode message {} at {}".format(self.buffer, self.offset - l))
     
-    def read220(self, l = 1):
-        try:
-            return base.b220decode(self.read(l))
-        except ValueError as e:
-            raise ValueError("Failed to decode message {} at {}".format(self.buffer, self.offset - l))
+    def write95(self, v, l = 1):
+        self.write(base.b95encode(v, l))
+    
     
     def read95Bytes(self, l = 1):
         try:
@@ -53,14 +70,45 @@ class FurcBuffer:
         except ValueError as e:
             raise ValueError("Failed to decode message {} at {}".format(self.buffer, self.offset - l))
     
+    def write95Bytes(self, data, l = 1):
+        if len(data)/95 > l:
+            raise ValueError("Can't pack data size of {} is greater than b95 size of {}".format(len(data)/95, l))
+        
+        self.write(base.b95encode(len(data), l))
+        self.write(data)
+    
+    
     def read95String(self, l = 1):
         return self.read95Bytes(l).decode()
+    
+    def write95String(self, v, l = 1):
+        return self.write95Bytes(v.encode(), l)
+    
+    
+    
+    def read220(self, l = 1):
+        try:
+            return base.b220decode(self.read(l))
+        except ValueError as e:
+            raise ValueError("Failed to decode message {} at {}".format(self.buffer, self.offset - l))
+    
+    def write220(self, v, l = 1):
+        self.write(base.b220encode(v, l))
+    
     
     def read220Bytes(self, l = 1):
         try:
             return self.read(base.b220decode(self.read(l)))
         except ValueError as e:
             raise ValueError("Failed to decode message {} at {}".format(self.buffer, self.offset - l))
+    
+    def write220Bytes(self, data, l = 1):
+        if len(data)/220 > l:
+            raise ValueError("Can't pack data size of {} is greater than b220 size of {}".format(len(data)/220, l))
+        
+        self.write(base.b220encode(len(data), l))
+        self.write(data)
+    
     
     def read220ByteArray(self, l = 1):
         result = []
@@ -72,5 +120,17 @@ class FurcBuffer:
         
         return bytes(result)
     
+    def write220ByteArray(self, v, l = 1):
+        if len(v) / 220 > l:
+            raise ValueError("Can't pack data size of {} is greater than b220 size of {}".format(len(data)/220, l))
+        
+        self.write220(len(v), l)
+        for i in v:
+            self.write220(v, 2)
+    
+    
     def read220String(self, l = 1):
         return self.read220Bytes(l).decode()
+    
+    def write220String(self, v, l = 1):
+        return self.write220Bytes(v.encode(), l)
